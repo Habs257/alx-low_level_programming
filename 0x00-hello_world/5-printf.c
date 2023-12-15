42871e6876bf415f809ab6984b08745fefe2a245
Habs257 <yonliaoussihabiboulaye@gmail.com>
2023-12-14 18:37:32 -0800

    fd

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-/** program that prints exactly followed by a new line
-* return : 0 (sucsses)
-*/
-int main (void)
+/**program that prints exactly followed by a new line*/
+int main(void)
 {
 puts("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
