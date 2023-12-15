010a475bd13eaa6147c936ea965d0028bbef00bf
Habs257 <yonliaoussihabiboulaye@gmail.com>
2023-12-14 18:00:31 -0800

    rt

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - A programe that print a fuction
+*return: 0 (sucsses)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
