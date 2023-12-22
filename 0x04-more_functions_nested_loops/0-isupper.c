39854e3c34ac05b5d32391c1e2a42568bc508d9c
Habs257 <yonliaoussihabiboulaye@gmail.com>
2023-12-21 17:29:54 -0800

    file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/* _isupper -prog ckek for uppercase charracter
+* c: variable 
+* return: 0.
+*/
+int _isupper(int C)
+{
+if (c >= 'A' && C <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
