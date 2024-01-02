b94e4ddaf6c17e959994786181720e669a4a2f6d
Habs257 <yonliaoussihabiboulaye@gmail.com>
2024-01-02 15:53:18 -0800

    tu

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,19 @@
 #include "main.h"
+
 /**
- * _strlen - return the length of a string
+ * _strelen - return the length of a string
  * @s: string
- * return: length
+ *
+ * Return: length
  */
 int _strelen(char *s)
 {
-	int longi = 0;
+int longi = 0;
 
-	while (*s != '\0')
-	{
-		longi++;
-		s++;
-	}
-
-	return (longi);
+while (*s != '\0')
+{
+longi++;
+s++;
+}
+return (longi);
 }
