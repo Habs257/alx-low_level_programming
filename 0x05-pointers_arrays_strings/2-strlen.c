6af04f29ad249979830ac4460dbcde778dac7f74
Habs257 <yonliaoussihabiboulaye@gmail.com>
2024-01-02 15:14:42 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - return the length of a string
+ * @s: string
+ * return: length
+ */
+int _strelen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '/0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
+}
