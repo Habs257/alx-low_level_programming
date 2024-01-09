95bbf45d95a656a5520bf8e08553131ecda5f140
Habs257 <yonliaoussihabiboulaye@gmail.com>
2024-01-09 14:01:20 -0800

    first

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+#include "unistd.h"
+/**
+ * _putchar - write the character c to stdout
+ * @c: the character to print
+ *
+ * return on succes 1.
+ * on error, -1 is returned 
+ * an error is set on propriatedly.
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+char *_ memset(char *s, char b, unsigned int n);
+in _putchar(char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+unsigned int *strpbrk(char *s, char *accept);
+char *strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print _diagsums(int *a, int size);
+void set_string(char **s, char *to);
+
+#endif
