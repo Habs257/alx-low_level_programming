be5a9065dbb9203acaea8b0f2929fd99058f497d
Habs257 <yonliaoussihabiboulaye@gmail.com>
2024-02-17 02:26:41 -0800

    the end

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,28 +1,20 @@
-#include <stdio.>
+#include "main.h"
 /**
-* _putchar - writes a character to stdout
-* @c: The character to print
-*
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriate
-**/
-int _putchar(char c)
-{
-return (putchar(c));
-}
-/**
- * main - check the code
- *
- * Return: Always 0.
+ * main - print _putchar
+ * is written by habson7
+ * Return: 0
  */
+
 int main(void)
 {
-char text[] = "_putchar";
-int i;
-for (i = 0; text[i] != '\0'; i++)
-{
-_putchar(text[i]);
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,25 +1,15 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
-* main - check the code.
-*
-* Return: Always 0.
-*/
-int main(void)
-{
-print_alphabet_x10();
-return (0);
-}
-/** Function to print the alphabet in lowercase,
- *
- * followed by a newline
+ * print_alphabet - print all alphabets in lowercase
  */
+
 void print_alphabet(void)
 {
-char letter = 'a';
-while (letter <= 'z')
-{
-_putchar(letter);
-letter++;
-}
-_putchar('\n');
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
 }
